deb7aa4726965e4c9e5719c1ac98fa1a85687be9
Wilfred Gitee <wilfred.gitee@gmail.com>
2022-03-30 15:24:13 +0300

    Finished MERN - Ecommerce App

---
+++ backend/data/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    name: 'NordicTrack T Series Treadmills',
+    image: 'https://m.media-amazon.com/images/I/81A3zAMBgNS._AC_SL1500_.jpg',
+    description:
+      '0% OneTouch incline control; 10 MPH SMART OneTouch speed control; With iFIT, your trainer auto-adjusts your speed and incline through a smart Bluetooth connection',
+    brand: 'NordicTrack',
+    category: 'Cardio Exercise',
+    price: 3799,
+    countInStock: 15,
+    rating: 5,
+    numReviews: 2,
+  },
+  {
+    name: 'Gym Fitness Equipment Spinning Bike',
+    image: 'https://sc04.alicdn.com/kf/H1245a091b842437383f1b3b2cc11c9726.jpg',
+    description:
+      'Designed to work with the Peloton and Zwift apps, allowing you to stream thousands of classes directly into your home and compete against others in a virtual world that motivates you at every mile.',
+    brand: 'Sports Pro Bike',
+    category: 'Cardio Training/ Spinning Bike',
+    price: 799,
+    countInStock: 49,
+    rating: 3,
+    numReviews: 74,
+  },
+  {
+    name: 'Balelinko Hex Dumbbells',
+    image: 'https://m.media-amazon.com/images/I/71ucIeqRqyL._AC_SL1500_.jpg',
+    description:
+      'Perfect for any Weight Training: For anyone who wants to build strength, burn fat, or create a tune body, free weight training with dumbbells are proved more effective.',
+    brand: 'Balelinko',
+    category: 'Weight & Accessories',
+    price: 49.99,
+    countInStock: 731,
+    rating: 5,
+    numReviews: 189,
+  },
+  {
+    name: 'Sunny Health & Fitness SF-RW1205 Rowing Machine Rower with 12 Level Adjustable Resistance, Digital Monitor and 220 LB Max Weight',
+    image: 'https://m.media-amazon.com/images/I/71aAnkeZExL._AC_SL1500_.jpg',
+    description:
+      'Achieve a smooth and enduring rowing workout with the SF-RW1205 Rowing Machine by Sunny Health & Fitness. The smooth glide seat and 12 levels of hydraulic resistance will allow you to complete a quiet and enduring ride, while working your full body, increasing your cardiovascular fitness, and burning calories. The pivoting foot pedals with foot straps are great for beginners or those with tight muscles, as they open your range of motion allowing you to achieve the safest posture throughout each phase of your row. Row in comfort with the cushioned seat and foam handlebars. When you’ve completed your workout, the compact design makes it easy to store. Level up your rowing experience with the SF-RW1205 Rowing Machine by Sunny Health & Fitness. ',
+    brand: 'Sunny Health & Fitness',
+    category: 'Health & Fitness',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
