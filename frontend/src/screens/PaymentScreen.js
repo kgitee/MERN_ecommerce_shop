fee8db0acbc13ede4e3d70dddfa0657edbbd513d
Wilfred Gitee <wilfred.gitee@gmail.com>
2022-03-31 14:47:29 +0300

    Updated payment method

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -40,14 +40,14 @@ const PaymentScreen = ({ history }) => {
               checked
               onChange={(e) => setPaymentMethod(e.target.value)}
             ></Form.Check>
-            {/* <Form.Check
+            {/*<Form.Check
               type='radio'
-              label='Stripe'
-              id='Stripe'
+              label='Cash'
+              id='Cash'
               name='paymentMethod'
-              value='Stripe'
+              value='Cash'
               onChange={(e) => setPaymentMethod(e.target.value)}
-            ></Form.Check> */}
+            ></Form.Check>*/}
           </Col>
         </Form.Group>
 
